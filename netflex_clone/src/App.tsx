54ec64beba6a386a485efa73844efa85124b1a63
gulggum <163222604+gulggum@users.noreply.github.com>
2024-11-21 14:48:30 +0900

    Router
    Home, Tv, Search 라우터

--- netflex_clone/src/App.tsx
+++ netflex_clone/src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+
 function App() {
   return (
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
