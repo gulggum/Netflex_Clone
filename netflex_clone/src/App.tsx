97119ea4baf59902b709b33940a0423881583770
gulggum <163222604+gulggum@users.noreply.github.com>
2024-11-21 14:38:19 +0900

    setting-finish
    gogo

--- netflex_clone/src/App.tsx
+++ netflex_clone/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>안녕</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
