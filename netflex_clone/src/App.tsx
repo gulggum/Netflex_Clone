b0a89cc6e54c62587906bec409f5836bdfab5896
gulggum <163222604+gulggum@users.noreply.github.com>
2024-11-21 14:41:49 +0900

    Update App.tsx

--- netflex_clone/src/App.tsx
+++ netflex_clone/src/App.tsx
@@ -3,11 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route></Route>
-      </Routes>
-    </BrowserRouter>
+
   );
 }
 
