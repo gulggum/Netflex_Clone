3db094774345554ea25e0fc21adc0961c21d7115
gulggum <163222604+gulggum@users.noreply.github.com>
2024-12-02 11:14:40 +0900

    home_slider
    버튼누르면 6개씩 슬라이딩되도록
    이어지게 슬라이드가 안되서 추후 수정예정,.,.

--- netflex_clone/src/Routes/Home.tsx
+++ netflex_clone/src/Routes/Home.tsx
@@ -2,6 +2,9 @@ import { fetchMovies, IGetMoviesResult } from "../api";
 import { useQuery } from "react-query";
 import { MakeImagePath } from "../utils";
 import styled from "styled-components";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
 const Wrapper = styled.div``;
 const Banner = styled.div<{ bgImage: string }>`
   display: flex;
@@ -24,18 +27,127 @@ const Overview = styled.p`
   line-height: 30px;
 `;
 
+const Slider = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  top: -100px;
+  button {
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 10px;
+    font-size: 20px;
+    border: none;
+    cursor: pointer;
+    z-index: 1;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.7);
+    }
+  }
+`;
+
+const SlideWrapper = styled(motion.div)`
+  padding: 0px 82px;
+  width: 100%;
+  display: flex;
+  gap: 8px;
+  position: absolute;
+`;
+const SlideBox = styled(motion.div)<{ bgImage: string }>`
+  width: 300px;
+  height: 180px;
+  background-color: white;
+  color: black;
+  background-image: url(${(props) => props.bgImage});
+  background-size: cover;
+  background-position: center ceter;
+  border-radius: 5px;
+  text-align: center;
+  line-height: 180px;
+  font-size: 18px;
+  font-weight: 600;
+  color: ${(props) => props.theme.white.lighter};
+`;
+
+const sliderVariant = {
+  start: (back: boolean) => ({
+    x: back ? -window.outerWidth : window.outerWidth,
+  }),
+  end: { x: 0 },
+  exit: (back: Boolean) => ({
+    x: back ? window.outerWidth : -window.outerWidth,
+  }),
+};
+
+const offset = 6;
+
 function Home() {
   const { isLoading, data } = useQuery<IGetMoviesResult>("movies", fetchMovies);
+  const [index, setIndex] = useState(0);
+  const [back, setBack] = useState(false);
+  const [leaving, setLeaving] = useState(false);
+
+  const onSliderPrev = () => {
+    if (data) {
+      if (leaving) return; //현재 애니메이션이 진행 중이면 클릭 무시
+      onExitComplete(); //애니메이션이 진행중임을 설정
+      setBack(true);
+      const totalMovies = data.results.length;
+      const maxIndex = Math.floor(totalMovies / offset);
+      setIndex((prev) => (prev - 1 + maxIndex) % maxIndex);
+    }
+  };
+  const onSliderNext = () => {
+    if (data) {
+      if (leaving) return;
+      onExitComplete();
+      setBack(false);
+      const totalMovies = data.results.length;
+      const maxIndex = Math.floor(totalMovies / offset);
+      setIndex((prev) => (prev + 1) % maxIndex); //index값은 0,1,2,3에서 순환 6개씩나오는 한 page가 1 index}
+    }
+  };
+  const onExitComplete = () => setLeaving(false); //애니메이션 완료후 leaving 상태를 false설정
+
   console.log(isLoading, data);
   return (
     <Wrapper>
       {isLoading ? (
         "Loding..."
       ) : (
-        <Banner bgImage={MakeImagePath(data?.results[0].backdrop_path || "")}>
-          <Title>{data?.results[0].title}</Title>
-          <Overview>{data?.results[0].overview}</Overview>
-        </Banner>
+        <>
+          <Banner bgImage={MakeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <AnimatePresence initial={false} onExitComplete={onExitComplete}>
+            <Slider>
+              <button onClick={onSliderPrev}>Prev</button>
+              <SlideWrapper
+                key={index}
+                custom={back}
+                variants={sliderVariant}
+                initial="start"
+                animate="end"
+                exit="exit"
+                transition={{ type: "tween", stiffness: 100, duration: 1 }}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <SlideBox
+                      bgImage={MakeImagePath(movie?.backdrop_path)}
+                      key={movie.id}
+                    >
+                      {movie.title}
+                    </SlideBox>
+                  ))}
+              </SlideWrapper>
+              <button onClick={onSliderNext}>Next</button>
+            </Slider>
+          </AnimatePresence>
+        </>
       )}
     </Wrapper>
   );
